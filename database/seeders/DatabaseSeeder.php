5fa743d3bdea2287da2957a164fb6c76a094aeb2
Roberto <roberto.britz@hotmail.com>
2026-03-02 14:30:31 +0000

    Created seeders and added images for the events

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,13 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
@@ -17,9 +15,17 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        //        User::factory()->create([
+        //            'name' => 'Test User',
+        //            'email' => 'test@example.com',
+        //        ]);
+
+        $this->call([
+            EventSeeder::class,
+            EventDaySeeder::class,
+            SpeakerSeeder::class,
+            TalkSeeder::class,
+            TicketSeeder::class,
         ]);
     }
 }
